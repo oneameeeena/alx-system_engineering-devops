2638797915c64fd41c48df11318b86f7af9bc64e
oneameeeena <aminaouaqqaouraho@gmail.com>
2024-07-30 08:41:15 -0700

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
-
-import re
+"""models doc"""
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+if __name__ == "__main__":
+    id_employee = int(sys.argv[1])
+
+    url = "https://jsonplaceholder.typicode.com"
+    url_user = requests.get(f"{url}/users/{id_employee}").json()
+    url_user_todos = requests.get(f"{url}/users/{id_employee}/todos").json()
+
+    username = url_user.get("username")
+
+    filename = f"{id_employee}.csv"
+    with open(filename, 'w', encoding="utf-8") as file:
+        for todos in url_user_todos:
+            data = f'"{id_employee}","{username}","{todos.get("completed")}",'
+            data2 = f'"{todos.get("title")}"\n'
+            file.write(data+data2)
